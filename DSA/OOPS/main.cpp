07c09dc2e74382082a2dc969d7d7a15c96ef30f4
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-08-21 23:16:07 +0530

    started :p

---
+++ DSA/OOPS/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Animal{
+    int num1;
+    int num2;
+    char ch;
+
+};
+
+
+int main(){
+    cout<<"The size of empty class is "<<sizeof(Animal);
+    //1 
+
+
+    return 0;
+}
