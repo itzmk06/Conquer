c9c492822296ece540815343dc3c477e7d7f57e8
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-08-21 23:42:51 +0530

    static way

--- DSA/OOPS/main.cpp
+++ DSA/OOPS/main.cpp
@@ -6,13 +6,33 @@ class Animal{
     int num2;
     char ch;
 
+    public:
+    string name;
+    int age;
+
+    void eat(){
+        cout<<"Eating!"<<endl;
+    }
+    void sleep(){
+        cout<<"Sleeping!"<<endl;
+    }
 };
 
 
 int main(){
-    cout<<"The size of empty class is "<<sizeof(Animal);
+    cout<<"The size of empty class is "<<sizeof(Animal)<<endl;
     //1 
 
+    //static allocation of objects 
+    Animal a1;
+    // to assign values in static way 
+    a1.name="Lion";
+    a1.age=20;
+
+    cout<<"Name of animal is "<<a1.name<<endl;
+    cout<<"Age of animal is "<<a1.age<<endl;
+    a1.eat();
+    a1.sleep();
 
     return 0;
 }
