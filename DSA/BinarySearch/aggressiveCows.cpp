657935e34fba8eb319678bcb30e8e8b1f2927393
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-11-21 22:40:15 +0530

    Update aggressiveCows.cpp

--- DSA/BinarySearch/aggressiveCows.cpp
+++ DSA/BinarySearch/aggressiveCows.cpp
@@ -83,3 +83,94 @@ int main() {
 }
 
 // } Driver Code Ends
+
+// * optimal approach 
+
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+
+    bool canIPlaceCows(vector<int>&stalls,int distance,int k){
+        // we'll place first cow at zeroth index
+        int count=1;
+        int lastPosition=stalls[0];
+        for(int i=1;i<stalls.size();i++){
+            if(stalls[i]-lastPosition>=distance){
+                // this means we can store the cow here 
+                // update cow position 
+                lastPosition=stalls[i];
+                count++;
+            }
+            // if we placed all cows return true 
+            if(count>=k){
+                return true;
+            }
+        }
+        // we're not able to place all cows return false 
+        return false;
+    }
+
+    int aggressiveCows(vector<int> &stalls, int k) {
+        // Write your code here
+        // sort the vector 
+        sort(stalls.begin(),stalls.end());
+        int minDistance=1;
+        int maxDistance=stalls[stalls.size()-1]-stalls[0];
+        int ans=0;
+        int beg=minDistance;
+        int end=maxDistance;
+        while(beg<end){
+            int mid=beg+(end-beg)/2;
+            if(canIPlaceCows(stalls,mid,k)){
+                ans=mid;
+                beg=mid+1;
+            }else{
+                end=mid;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int k;
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string in;
+        getline(cin, in);
+        stringstream sss(in);
+        int num;
+        while (sss >> num) {
+            k = num;
+        }
+        Solution ob;
+        int ans = ob.aggressiveCows(arr, k);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
