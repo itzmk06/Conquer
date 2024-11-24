38177aa94ef7c24b21768d5ed3011dbb894536d1
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-11-24 22:23:32 +0530

    added optimal for maj ele 2

--- DSA/Arrays/Questions/majorityElement2.cpp
+++ DSA/Arrays/Questions/majorityElement2.cpp
@@ -21,3 +21,52 @@ public:
 
     }
 };
+
+// * optimal 
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int count1=0;
+        int count2=0;
+        int maj1=-1;
+        int maj2=-1;
+        for(int num:nums){
+            if(maj1==num){
+                count1++;
+            }else if(maj2==num){
+                count2++;
+            }else if(count1==0){
+                maj1=num;
+                count1=1;
+            }else if(count2==0){
+                maj2=num;
+                count2=1;
+            }else {
+                count1--;
+                count2--;
+            }
+        }
+
+
+        int freq1=0;
+        int freq2=0;
+        for(int num:nums){
+            if(num==maj1){
+                freq1++;
+            }
+            if(num==maj2){
+                freq2++;
+            }
+        }
+        vector<int>ans;
+
+        if(freq1>(nums.size()/3)){
+            ans.push_back(maj1);
+        }
+         if(freq2>(nums.size()/3)&&maj1!=maj2){
+            ans.push_back(maj2);
+        }
+        return ans;
+
+    }
+};
