8a3b2bc298d15ce8564b576a5bd39f075fadd734
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-11-24 20:44:01 +0530

    Update majority_element.cpp

--- DSA/Arrays/Questions/majority_element.cpp
+++ DSA/Arrays/Questions/majority_element.cpp
@@ -17,3 +17,29 @@ public:
         return maxElement;
     }
 };
+
+// * optimal 
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int majority=NULL;
+        // traverse whole array 
+        for(int num:nums){
+            // if count = 0, assume that element as majority 
+            if(count==0){
+                majority=num;
+                // increase the count 
+                count=1;
+            }else if(num==majority){
+                // increase count
+                count++;
+            }else{
+                // this is new element 
+                // decrement count 
+                count--;
+            }
+        }
+        return majority;
+    }
+};
