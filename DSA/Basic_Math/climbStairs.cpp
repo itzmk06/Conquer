de24e23903b7259667f87e5a9aa7adcc486b9d9f
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-12-14 14:32:03 +0530

    Update climbStairs.cpp

--- DSA/Basic_Math/climbStairs.cpp
+++ DSA/Basic_Math/climbStairs.cpp
@@ -18,3 +18,21 @@ public:
         return fn(n,dp);
     }
 };
+
+// * using table way 
+class Solution {
+public:
+    int fn(int n,vector<int>&dp) {
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    int climbStairs(int n){
+        vector<int>dp(n+1,-1);
+        // intialization 
+        dp[0]=1;
+        dp[1]=1;
+        return fn(n,dp);
+    }
+};
