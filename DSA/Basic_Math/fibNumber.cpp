98e9d2108521b8ee84d6358d7826774149a2c765
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-12-14 11:29:19 +0530

    Update fibNumber.cpp

--- DSA/Basic_Math/fibNumber.cpp
+++ DSA/Basic_Math/fibNumber.cpp
@@ -12,3 +12,21 @@ public:
         return n1;
     }
 };
+
+// * using memoization 
+class Solution {
+public:
+    int fn(int n,vector<int>&dp) {
+        if(n<=1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=fn(n-1,dp)+fn(n-2,dp);
+    }
+    int fib(int n){
+        vector<int>dp(n+1,-1);
+        return fn(n,dp);
+    }
+};
