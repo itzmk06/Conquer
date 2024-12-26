5b776eca983fa3e7de9392f3f71da912106e7ef7
Manoj Kumar M <141392608+itzmk06@users.noreply.github.com>
2024-12-26 11:53:58 +0530

    Create number_of_ways_to_split_array.cpp

---
+++ DSA/Arrays/Questions/number_of_ways_to_split_array.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    int waysToSplitArray(vector<int>& nums) {
+        // get total sum 
+        long long total_sum=0;
+        for(auto i:nums){
+            total_sum+=i;
+        }
+        // get left sum 
+        int count=0;
+        long long left_sum=0;
+        for(int i=0;i<nums.size()-1;i++){
+            left_sum+=nums[i];
+            // get right sum 
+            long long right_sum=total_sum-left_sum;
+            if(left_sum>=right_sum){
+                count++;
+            }
+        }
+        return count;
+    }
+};
